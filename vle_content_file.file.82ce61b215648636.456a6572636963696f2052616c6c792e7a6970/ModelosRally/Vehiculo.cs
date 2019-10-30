6456b701f24b90ee050f59fe98478eb2129d9b41
cristobal maggi <camaggi1@miuandes.cl>
2019-10-29 21:26:42 -0400

    commit 00

---
+++ vle_content_file.file.82ce61b215648636.456a6572636963696f2052616c6c792e7a6970/ModelosRally/Vehiculo.cs
@@ -0,0 +1,25 @@
+﻿namespace ModelosRally
+{
+    public class Vehiculo
+    {
+        string marca;
+        int cilindrada;
+
+        public Vehiculo(string marca, int cilindrada)
+        {
+            this.marca = marca;
+            this.cilindrada = cilindrada;
+        }
+
+        public string getMarca()
+        {
+            return marca;
+        }
+
+        public int getCilindrada()
+        {
+            return cilindrada;
+        }
+
+    }
+}
